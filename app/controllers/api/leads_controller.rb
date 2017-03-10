d1ae6c51075db0ff4ad3298bbaba3acfdf10572c
Jordan Etzler <jozza24@gmail.com>
2017-03-11 02:56:30 +1100

    Email Notifications Setup

--- app/controllers/api/leads_controller.rb
+++ app/controllers/api/leads_controller.rb
@@ -1,8 +1,8 @@
+require 'mail'
 
 module Api
 
   class LeadsController < ApplicationController
-    require 'mail'
 
     def new
     end
@@ -15,11 +15,31 @@ module Api
       @lead.created_at = (Time.zone = "EST")
       @lead.save
 
-      ticket = ::MailSend.new
-      puts ticket
-
       current_id = @lead.id
       current_name = @lead.name
+
+      name = params[:name].to_str
+      email = params[:email].to_str
+      phone_number = params[:phone].to_str
+
+      # Email Lead
+      mail = Mail.new do
+        from    'lessons@exceldrive.com'
+        to      'jozza24@gmail.com'
+        subject 'Excel Drive Landing Page has a new Lead'
+
+        html_part do
+          content_type 'text/html; charset=UTF-8'
+          body "<h1>Excel Drive has a New Lead</h1><br>
+                  <p><b>Name : </b>#{name}</p><br>
+                  <p><b>Email : </b>#{email}</p><br>
+                  <p><b>Phone Number : </b>#{phone_number}</p><br>"
+        end
+
+      end
+
+      mail.delivery_method :sendmail
+      mail.deliver!
 
       render json: { current_id: current_id, current_name: current_name }
 
